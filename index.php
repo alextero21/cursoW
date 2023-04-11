cbdb768dd6411335d14d4021394f8c7db0c3a04d
Alexis Quintero <alextero.to@gmail.com>
2023-04-11 02:10:57 -0500

    End project V1

--- index.php
+++ index.php
@@ -4,40 +4,62 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Portafolio</title>
+  
   <link rel="stylesheet" href="css/index.css">
+  <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
 </head>
 <body>
  
 <header>
-  <nav class="main-nav">
-  <div class="logo"><img src="img/slogant2.png" alt=""></div>
+  <nav class="main-nav" >
+    
+   <div class="logo"><img src="img/slogan16.png" alt=""></div>
+   <div class="menu-toggle"><i class="icon-align-justify"></i></div>
 
-    <ul>
-    </ul>
 
-    <ul>
-    </ul>
+    <div class='nav_menu'>
+    </div>
+
+    <div class='nav_menu'>
+    </div>
+
 
     <ul>
       <li><a href="#" class="specialBtn">Inicio</a></li>
-      <li><a href="#about" class="about">Sobre mí</a></li>
-      <li><a href="#">Servicios</a></li>
+      <li><a href="#service">Servicios</a></li>
       <li><a href="#contact">Contacto</a></li>
     </ul>
+
+
+    
+    
+
+    
+
   </nav>
+  <div class="intro">
+    <h1>Diseños personalizados</h1>
+    
+    <p>😃Ofrecemos servicios de desarrollo web, diseño web y creación de proyectos personalizados para el camino hacia el éxito en línea🟢. ¡<a href="#contact">Contáctanos</a> hoy mismo para comenzar a trabajar juntos! 🦾</p>
+   
+  </div>
+
+
 
 
 </header>
 
   <main>
 
-    <section class="servicios">
+    <section id="service" class="servicios">
+
       <div class="servicios__titulo">
         <h2>Nuestros Servicios</h2>
         <p>Conoce lo que podemos ofrecerte</p>
       </div>
 
       <div class="servicios__contenedor">
+
         <div class="servicios__item">
           <div class="servicios__icono">
             <i class="fas fa-paint-brush"></i>
@@ -58,10 +80,13 @@
           <div class="servicios__icono">
             <i class="fas fa-chart-bar"></i>
           </div>
-          <h3 class="servicios__nombre">Marketing Digital</h3>
-          <p class="servicios__descripcion">Diseñamos estrategias de marketing digital para aumentar el alcance de tu negocio.</p>
+          <h3 class="servicios__nombre">Creación de proyectos</h3>
+          <p class="servicios__descripcion">Transformamos tus ideas en realidad, creando soluciones innovadoras y personalizadas. </p>
         </div>
       </div>
+
+
+
     </section>
   
 
@@ -69,23 +94,24 @@
     <section id="contact">
 
       <!-- <h2>Contáctanos</h2> -->
-      <div class='imgWomenContact'></div>
-    
-       
-      
 
       <div class="container">
+
         <div class="img_form">
-          <img src="img/handWomen.png" height="500px" alt="Imagen">
+          <img src="img/handWomen.png" height="350px" alt="Imagen">
         </div>
+
+
         <div class="contact_form">
           <form method="POST" action="app/email.php" >
+
+            <h2>Contáctanos</h2>
             <div class="form-group" >
               <label for="name">Nombre:</label>
               <input type="text" id="name" name="name" required >
             </div>
             <div class="form-group">
-              <label for="email">Email:</label>
+              <label for="email">Correo:</label>
               <input type="email" id="email" name="email" required >
             </div>
             <div class="form-group">
@@ -104,7 +130,17 @@
 
 
   
+<script>
+  const menuToggle = document.querySelector('.menu-toggle');
+  const nav = document.querySelector('nav ul');
 
+  menuToggle.addEventListener('click', () => {
+  nav.classList.toggle('slide');
+  nav.classList.contains('slide');
+});
+
+
+</script>
   
 
 
